64f33ad0bbd06e3e869eed9c67dcb00aad3b0b5f
Krystof Beuermann <kreiselkreisch@gmx.de>
2017-01-13 09:04:11 +0100

    Fehlerkorrektur

--- Config/deploy/Production.swift
+++ Config/deploy/Production.swift
@@ -3,6 +3,6 @@ import Flock
 class Production: Configuration {
     func configure() {
 		// Config.SSHAuthMethod = .key("/path/to/mykey")
-    Servers.add(ip: "45.76.84.134", user: "root", roles: [.app], authMethod: .key("/Users/krystof/.ssh/id_rsa"))
+    Servers.add(ip: "krystof.eu", user: "krystof", roles: [.app], authMethod: .key("/Users/krystof/.ssh/id_rsa"))
     }
 }
