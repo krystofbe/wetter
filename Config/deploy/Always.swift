d73913908f9dfbe4f21f71c390d7e72fb87ea5b8
Krystof Beuermann <kreiselkreisch@gmx.de>
2017-04-02 10:49:39 +0200

    Change typo in Always

--- Config/deploy/Always.swift
+++ Config/deploy/Always.swift
@@ -6,7 +6,7 @@ class Always: Configuration {
     Config.projectName = "muensterwetter"
 		Config.executableName = "App" // Same as Config.projectName unless your project is divided into modules
 		Config.repoURL = "https://github.com/krystofbe/muensterwetter.git"
-		Config.directory = "/var/www/muensterwetter/current/" 	
+		Config.currentDirectory = "/var/www/muensterwetter/current/" 	
 		// Optional config:
 		// Config.deployDirectory = "/var/www"
 		// Config.swiftVersion = "https://swift.org/builds/swift-3.0-release/ubuntu1510/swift-3.0-RELEASE/swift-3.0-RELEASE-ubuntu15.10.tar.gz"
